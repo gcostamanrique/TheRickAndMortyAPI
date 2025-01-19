9bb2dd3e761b34d49b886e2d78a77d61704099d3
Guilherme Costa Manrique <gcostamanrique@gmail.com>
2025-01-19 13:51:16 -0300

    Atualização

--- TheRickAndMortyAPI/TheRickAndMortyAPIVisualizacao/obj/Debug/net8.0/TheRickAndMortyAPIVisualizacao.AssemblyInfo.cs
+++ TheRickAndMortyAPI/TheRickAndMortyAPIVisualizacao/obj/Debug/net8.0/TheRickAndMortyAPIVisualizacao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheRickAndMortyAPIVisualizacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46fb2bf57c50d6debe24ce8260fdb157d5b5c2eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982653c92e35e06917cb867d7efebdaceaaa24b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheRickAndMortyAPIVisualizacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheRickAndMortyAPIVisualizacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
